82e5d15914a2cc1d9cdc37255730e247c44897f6
Niklas Mårdby <niklas.mardby@zocom.se>
2023-02-06 09:56:28 +0100

    First session over

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -1,7 +1,10 @@
 
 // vår template för alla framtida objekt av Monster-klassen
 class Monster {
+    // egenskaper och metoder kan vara public eller private
+
     private name: string;  // egenskap, property = variabel inne i objekt
+    // fler egenskaper??
 
     constructor(name: string) {  // konstruktor = funktionen som körs när vi skriver new Monster
         this.name = name;
@@ -10,6 +13,10 @@ class Monster {
     getName(): string {  // metod, method = funktion inne i ett objekt
         return this.name;
     }
+    setName(newName: string): void {
+        this.name = newName;
+    }
+    // fler metoder??
 }
 
 const n = new Monster("Niklas");
